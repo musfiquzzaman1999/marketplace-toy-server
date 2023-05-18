26bbc51ff41ae2b439277bcb65a5a5c925a5010d
unknown <musfiquzzama1999@gmail.com>
2023-05-18 22:55:25 +0600

    2nd commit

--- index.js
+++ index.js
@@ -29,6 +29,19 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    const toyCollection = client.db("toyMarketPlace").collection("toys");
+    
+
+    app.get('/toys', async(req,res)=>{
+        const cursor =toyCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
